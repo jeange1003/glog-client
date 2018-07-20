05772b2b6c8f66cc14d96a4059fa125c90679d41
JeanGe <jeange1003@163.com>
2018-07-20 23:24:14 +0800

    add base in route config for deploy in subdirectory

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ export function createRouter() {
     mode: 'history',
     fallback: false,
     scrollBehavior: () => ({ y: 0 }),
+    base: process.env.NODE_ENV === 'production' ? 'blog' : '',
     routes: [
       { path: '/', component: Home },
       { path: '/detail/:filePath', component: Detail }
