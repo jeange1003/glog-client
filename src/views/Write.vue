2fa0d1717792deb47cb5a318f3ff4724f28c8a0d
JeanGe <jeange1003@163.com>
2018-09-16 19:02:07 +0800

    preview in write

--- src/views/Write.vue
+++ src/views/Write.vue
@@ -1,18 +1,28 @@
 <template>
-    <div>
+    <div class="write">
         标题：<input type="text" v-model="title" />
-        内容：<textarea v-model="content"></textarea>
+        <div class="content">
+        内容：<textarea v-model="content" rows="50"></textarea>
+        预览：<div class="preview" v-html="previewContent"></div>
+        </div>
         <button @click="save">保存</button>
     </div>
 </template>
 
 <script>
+import showdown from 'showdown';
+const converter = new showdown.Converter();
 export default {
   data() {
     return {
       title: '',
       content: ''
     };
+  },
+  computed: {
+      previewContent() {
+          return converter.makeHtml(this.content)
+      }
   },
   methods: {
     save() {
@@ -26,5 +36,17 @@ export default {
 };
 </script>
 
-<style>
+<style lang="stylus" scoped>
+.write
+  display flex
+  flex-direction column
+  .content
+    display flex
+    > *
+      width 45%
+      padding 2em
+    textarea
+      border 1px solid #DDD
+    .preview
+      border 1px solid #DDD
 </style>
